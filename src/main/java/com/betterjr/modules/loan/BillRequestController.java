5b85fa22f08a1b3d3d6c383aaba47eb49d75df5a
Tangzw <tangzw@bytter.com>
2016-10-20 20:42:31 +0800

    微信接口

--- src/main/java/com/betterjr/modules/loan/BillRequestController.java
+++ src/main/java/com/betterjr/modules/loan/BillRequestController.java
@@ -14,30 +14,40 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.betterjr.common.utils.Collections3;
 import com.betterjr.common.web.AjaxObject;
+import com.betterjr.common.web.ControllerExceptionHandler;
 import com.betterjr.common.web.Servlets;
+import com.betterjr.common.web.ControllerExceptionHandler.ExceptionHandler;
 
 @Controller
-@RequestMapping(value = "/Wechat/Scf/BillRequest")
-public class WechatRequestController {
-    private static final Logger logger = LoggerFactory.getLogger(WechatRequestController.class);
-
+@RequestMapping(value = "/Scf/BillRequest")
+public class BillRequestController {
+    private static final Logger logger = LoggerFactory.getLogger(RequestController.class);
+    
     @Reference(interfaceClass = IScfBillRequestService.class)
     private IScfBillRequestService billRequestService;
-
+    
+    @RequestMapping(value = "/queryBillRequestList", method = RequestMethod.POST)
+    public @ResponseBody String queryRequestList(HttpServletRequest request, int flag, int pageNum, int pageSize) {
+        Map<String, Object> map = Servlets.getParametersStartingWith(request, "");
+        String[] queryTerm = new String[] { "custNo", "coreCustNo", "factorNo", "GTEactualDate", "LTEactualDate" };
+        Map<String, Object> qyMap = Collections3.filterMap(map, queryTerm);
+        return ControllerExceptionHandler.exec(new ExceptionHandler() {
+            public String handle() {
+                return billRequestService.webQueryBillRequestList(qyMap, flag, pageNum, pageSize);
+            }
+        }, "查询融资申请列表", logger);
+    }
     
     @RequestMapping(value = "/addBillRequest", method = RequestMethod.POST)
     public @ResponseBody String addBillRequest(HttpServletRequest request) {
         Map<String, Object> map = Servlets.getParametersStartingWith(request, "");
         logger.info("添加票据融资申请，入参:" + map.toString());
-
-        try {
-            return billRequestService.webAddBillRequest(map);
-        }
-        catch (Exception ex) {
-            logger.error("添加票据融资申请:", ex);
-            return AjaxObject.newError("addBillRequest service failed" + ex.getMessage()).toJson();
-        }
-
+        
+        return ControllerExceptionHandler.exec(new ExceptionHandler() {
+            public String handle() {
+                return billRequestService.webAddBillRequest(map);
+            }
+        }, "加票据融资申请", logger);
     }
     
     @RequestMapping(value = "/requestByOffer", method = RequestMethod.POST)
@@ -45,33 +55,13 @@ public class WechatRequestController {
         Map<String, Object> map = Servlets.getParametersStartingWith(request, "");
         logger.info("根据报价信息进行融资申请，入参:" + map.toString());
 
-        try {
-            return billRequestService.webRequestByOffer(offerId);
-        }
-        catch (Exception ex) {
-            logger.error("根据报价信息进行融资申请:", ex);
-            return AjaxObject.newError("requestByOffer service failed").toJson();
-        }
-
+        return ControllerExceptionHandler.exec(new ExceptionHandler() {
+            public String handle() {
+                return billRequestService.webRequestByOffer(offerId);
+            }
+        }, "根据报价信息进行融资申请", logger);
     }
 
-    @RequestMapping(value = "/queryRequestList", method = RequestMethod.POST)
-    public @ResponseBody String queryBillRequestList(HttpServletRequest request, int flag, int pageNum, int pageSize) {
-        Map<String, Object> map = Servlets.getParametersStartingWith(request, "");
-        String[] queryTerm = new String[] { "businStatus"};
-        map = Collections3.filterMap(map, queryTerm);
-        logger.info("查询票据融资申请，入参:" + map.toString());
-
-        try {
-            return billRequestService.webQueryBillRequestList(map, flag, pageNum, pageSize);
-        }
-        catch (Exception ex) {
-            logger.error("查询票据融资申请:", ex);
-            return AjaxObject.newError("queryRequestList service failed").toJson();
-        }
-
-    }
-    
     @RequestMapping(value = "/findRequestByBill", method = RequestMethod.POST)
     public @ResponseBody String findRequestByBill(HttpServletRequest request, String billId) {
         Map<String, Object> map = Servlets.getParametersStartingWith(request, "");
@@ -101,5 +91,4 @@ public class WechatRequestController {
         }
 
     }
-    
 }
